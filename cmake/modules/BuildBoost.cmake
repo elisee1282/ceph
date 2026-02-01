63ea72ad0acb675502713d10659af2b61fed0df9
elisee1282 <129553129+elisee1282@users.noreply.github.com>
2026-02-01 07:55:34 +0100

    Update BuildBoost.cmake
    https://download.ceph.com/qa/boost_${boost_version_underscore}.tar.bz2
    
    Signed-off-by: elisee1282 <129553129+elisee1282@users.noreply.github.com>

--- cmake/modules/BuildBoost.cmake
+++ cmake/modules/BuildBoost.cmake
@@ -169,7 +169,7 @@ function(do_build_boost root_dir version)
     list(APPEND boost_url
       https://download.ceph.com/qa/boost_${boost_version_underscore}.tar.bz2
       https://archives.boost.io//release/${boost_version}/source/boost_${boost_version_underscore}.tar.bz2
-      https://boostorg.jfrog.io/artifactory/main/release/${boost_version}/source/boost_${boost_version_underscore}.tar.bz2)
+      https://download.ceph.com/qa/boost_${boost_version_underscore}.tar.bz2)
     set(source_dir
       URL ${boost_url}
       URL_HASH SHA256=${boost_sha256}
